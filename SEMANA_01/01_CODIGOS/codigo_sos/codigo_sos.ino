75992acb96b36d80be47e4092f9e360eae85e37d
sophia.nobrega@sou.inteli.edu.br <sophia.nobrega@sou.inteli.edu.br>
2023-10-17 11:32:02 -0300

    codigo_sos

---
+++ SEMANA_01/01_CODIGOS/codigo_sos/codigo_sos.ino
@@ -0,0 +1,56 @@
+void setup() {
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+
+}
+
+void loop() {
+  //s
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+  //o
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(700);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(700);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(700);
+  digitalWrite(13, LOW);
+
+  //s
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+  delay(400);
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+
+}
